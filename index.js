e46b64c933accf92256c9b49978a4215068a2a7d
yehara <yoshinori.ehara@gmail.com>
2019-01-10 17:53:18 +0900

    ハッシュ計算の時間を計測するように

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "rust-wasm-hash";
+import jsSHA from "jssha";
 
-wasm.greet();
+var n = 10000000;
+
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+  return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}
+
+function measure(func) {
+  var start = performance.now();
+  func();
+  var end = performance.now();
+  console.log(end - start);
+}
+
+function wasm_hash() {
+  console.log(wasm.hash(n));
+}
+
+
+function js_hash() {
+  var sha = new jsSHA("SHA-1", "ARRAYBUFFER");
+  var input = new Uint8Array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]).buffer;
+  for(var i=0; i<n; ++i) { 
+    sha.update(input);
+  }
+  var hash = sha.getHash("ARRAYBUFFER");
+  console.log(buf2hex(hash));
+}
+
+measure(wasm_hash);
+measure(js_hash);
+
